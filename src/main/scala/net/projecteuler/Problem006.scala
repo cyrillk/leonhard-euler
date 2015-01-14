f3c9b0fa044095400dc848bb0087fecdefe9fc6b
Kirill Kulikov <kirill.kulikov@gmail.com>
2015-01-14 11:29:08 +0000

    problem 6

---
+++ src/main/scala/net/projecteuler/Problem006.scala
@@ -0,0 +1,23 @@
+package net.projecteuler
+
+import scala.annotation.tailrec
+
+/**
+  * Sum square difference
+  */ 
+object Problem006 extends AbstractProblem {
+
+  private def calculate(range: Seq[Int]):Int = {
+    val z = range zip range.map(Math.pow(_, 2).toInt)
+
+    z.foldLeft((0,0)) {
+      case (a, b) =>
+        (a._1 + b._1, a._2 + b._2)
+    } match {
+      case (a, b) =>
+        Math.pow(a, 2).toInt - b
+    }
+  }
+
+  override def result = calculate((1 to 100))
+}
