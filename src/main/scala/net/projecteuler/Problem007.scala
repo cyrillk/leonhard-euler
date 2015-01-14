ccec4ec6566d57c35929ac2e12782d12c3fc533b
Kirill Kulikov <kirill.kulikov@gmail.com>
2015-01-14 12:50:34 +0000

    problem 7

---
+++ src/main/scala/net/projecteuler/Problem007.scala
@@ -0,0 +1,36 @@
+package net.projecteuler
+
+import math._
+import scala.annotation.tailrec
+
+/**
+ * Problem 7 - 10001st prime
+ */
+object Problem007 extends AbstractProblem {
+
+  private def isPrime(p: Long): Boolean = {
+    p match {
+      case 0 => false // to catch 0
+      case 2 => true // to catch 2
+      case p if (p % 2 != 0) => // to catch odd numbers
+        val root = ceil(sqrt(p)).toLong
+          (3L to root).filter(s => p % s == 0).isEmpty
+      case _ => false
+    }
+  }
+
+  @tailrec
+  private def calculate(current: Long, required: Long, num: Long): Long = {
+
+    if (num > Long.MaxValue / 2) System.exit(0) // emergency exit
+
+    isPrime(num) match {
+      case true =>
+        if (current >= required) num else calculate(current + 1, required, num + 1)
+      case false =>
+        calculate(current, required, num + 1)
+    }
+  }
+
+  override def result = calculate(0L, 10001L, 0L)
+}
