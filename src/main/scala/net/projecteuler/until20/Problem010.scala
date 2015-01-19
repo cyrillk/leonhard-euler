f201b5457ac7d95a9d722a4836568b5e2e00e0d6
Kirill Kulikov <kirill.kulikov@gmail.com>
2015-01-19 10:06:40 +0000

    problem 10

---
+++ src/main/scala/net/projecteuler/until20/Problem010.scala
@@ -0,0 +1,28 @@
+package net.projecteuler.until20
+
+import net.projecteuler.AbstractProblem
+import math._
+import scala.annotation.tailrec
+
+/**
+ * Problem 10 - ???
+ */
+object Problem010 extends AbstractProblem {
+
+  private def isPrime(p: Long): Boolean = {
+    p match {
+      case 2 => true // to catch 2
+      case p if (p % 2 != 0) => // to catch odd numbers
+        val root = ceil(sqrt(p)).toLong
+        (3L to root).filter(s => p % s == 0).isEmpty
+      case _ => false
+    }
+  }
+
+  private final def Limit = 2000000L
+
+  override def result = (2L until Limit).foldLeft(0L){
+    case (a, b) if (isPrime(b)) => a + b
+    case (a, _) => a
+  }
+}
