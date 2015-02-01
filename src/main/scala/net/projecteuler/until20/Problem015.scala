3959659af37ebccfc1ae23b7b7a7aa1ead7fef36
Kirill Kulikov <kirill.kulikov@gmail.com>
2015-02-01 00:01:04 +0000

    problem 15

---
+++ src/main/scala/net/projecteuler/until20/Problem015.scala
@@ -0,0 +1,19 @@
+package net.projecteuler.until20
+
+import net.projecteuler.AbstractProblem
+import math._
+import scala.annotation.tailrec
+
+/**
+ * Problem 15 - Lattice paths
+ *
+ *
+ */
+object Problem015 extends AbstractProblem {
+
+  private def fact(num: Int): BigInt = (1 to num).toSeq.foldLeft(BigInt(1))((a, b) => a * b)
+
+  private def calc(n: Int, r: Int): BigInt = fact(n) / (fact(r) * fact(n - r))
+
+  override def result = calc(40, 20)
+}
